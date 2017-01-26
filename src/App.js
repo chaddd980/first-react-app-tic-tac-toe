d0869cbaa4e6b2f9ffaa6b434fce084201686fa2
chadd williams <chadd980@hotmail.com>
2017-01-25 21:15:21 -0500

    improved ai, added winning and tei messages

--- src/App.js
+++ src/App.js
@@ -5,26 +5,14 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      PLAYER_ONE_SYMBOL: "X",
-      COMPUTER_SYMBOL: "O",
+      PLAYER_ONE_SYMBOL: "",
+      COMPUTER_SYMBOL: "",
       board: [
         "", "", "", "", "", "", "", "", ""
       ],
-      currentTurn: "X",
-      game: "on"
+      currentTurn: "",
+      game: "off"
     }
-  }
-
-
-  handleClick(index) {
-    if (this.state.board[index] === "") {
-      this.state.board[index] = this.state.currentTurn
-      this.setState({
-        board: this.state.board,
-        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.COMPUTER_SYMBOL : this.state.PLAYER_ONE_SYMBOL,
-      })
-    }
-    this.checkWinner()
   }
 
   handleSelectionX() {
@@ -43,35 +31,19 @@ class App extends Component {
     })
   }
 
-  checkWinner() {
-    var combos = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8], [2,4,6], [3,4,5], [6,7,8]]
-    combos.find(function(combo) {
-      if(this.state.board[combo[0]] !== "" && this.state.board[combo[0]] === this.state.board[combo[1]] && this.state.board[combo[1]] === this.state.board[combo[2]]) {
-        alert("Player " + this.state.currentTurn + " wins!!")
-        this.setState({
-          game: "over",
-          board: [
-            "", "", "", "", "", "", "", "", ""
-          ],
-        })
-        return this.state.board[combo[0]]
-      } else {
-        return false
-      }
-    }.bind(this))
-  }
-
-  setTimer() {
-    if (this.state.game === "on") {
-      setTimeout(this.updateBoard.bind(this), 2000)
+  handleClick(index) {
+    if (this.state.board[index] === "" && this.state.PLAYER_ONE_SYMBOL !== "") {
+      let currentBoard = this.state.board
+      currentBoard.splice(index, 1, this.state.currentTurn)
+      this.setState({
+        game: "on",
+        board: currentBoard,
+      }, () => {
+        this.checkPlayer()
+      })
     }
   }
 
-  componentDidUpdate() {
-    if (this.state.game === "on" && this.state.currentTurn === this.state.COMPUTER_SYMBOL) {
-      this.setTimer()
-    }
-  }
 
   updateBoard() {
     var count = 0
@@ -82,15 +54,121 @@ class App extends Component {
         currentBoard.splice(index, 1, this.state.currentTurn)
         this.setState({
           board: currentBoard,
-          currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.COMPUTER_SYMBOL : this.state.PLAYER_ONE_SYMBOL
+        }, () => {
+          this.checkComputer()
         })
         count += 1
       } else {
         index = Math.round(Math.random()*8)
       }
     }
-
   }
+
+  checkPlayer() {
+    var combos = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8], [2,4,6], [3,4,5], [6,7,8]]
+    combos.find(function(combo) {
+      if(this.state.board[combo[0]] !== "" && this.state.board[combo[0]] === this.state.board[combo[1]] && this.state.board[combo[1]] === this.state.board[combo[2]]) {
+        alert("Player Wins!!!")
+        this.setState({
+          game: "over",
+          board: [
+            "", "", "", "", "", "", "", "", ""
+          ],
+          PLAYER_ONE_SYMBOL: "",
+          COMPUTER_SYMBOL: "",
+          currentTurn: ""
+        })
+        return this.state.board[combo[0]]
+      } else if (this.checkTie()) {
+        alert("Game is Tied!!!")
+        this.setState({
+          game: "over",
+          board: [
+            "", "", "", "", "", "", "", "", ""
+          ],
+          PLAYER_ONE_SYMBOL: "",
+          COMPUTER_SYMBOL: "",
+          currentTurn: ""
+        })
+      }
+    }.bind(this))
+    if (this.state.game !== "over") {
+      this.setState({
+        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.COMPUTER_SYMBOL : this.state.PLAYER_ONE_SYMBOL
+      }, () => {
+        this.setTimer()
+      })
+    }
+  }
+// use a variable like count in the checkplayer method. then in the else if check for checkTie and if count is 0
+  checkTie() {
+    let count = 0
+    this.state.board.map(function(cell,index){
+      if (cell === "") {
+        count += 1
+      }
+      return true
+    })
+    if (count === 0) {
+      return true
+    }
+  }
+
+  // componentWillMount() {
+  //   if (this.state.game === "on") {
+  //     var count = 0
+  //     this.state.board.map(function(cell,index){
+  //       if (cell === "") {
+  //         count += 1
+  //       }
+  //       return true
+  //     })
+  //     if (count === 0) {
+  //       alert("Game is Tied!!!")
+  //       this.setState({
+  //         game: "over",
+  //         board: [
+  //           "", "", "", "", "", "", "", "", ""
+  //         ],
+  //         PLAYER_ONE_SYMBOL: "",
+  //         COMPUTER_SYMBOL: "",
+  //         currentTurn: ""
+  //       })
+  //     }
+  //   }
+  // }
+
+  checkComputer() {
+    var combos = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8], [2,4,6], [3,4,5], [6,7,8]]
+    combos.find(function(combo) {
+      debugger
+      if(this.state.board[combo[0]] !== "" && this.state.board[combo[0]] === this.state.board[combo[1]] && this.state.board[combo[1]] === this.state.board[combo[2]]) {
+        alert("Computer Wins!!!")
+        this.setState({
+          game: "over",
+          board: [
+            "", "", "", "", "", "", "", "", ""
+          ],
+          PLAYER_ONE_SYMBOL: "",
+          COMPUTER_SYMBOL: "",
+          currentTurn: ""
+        })
+        return this.state.board[combo[0]]
+      }
+    }.bind(this))
+    if (this.state.game !== "over") {
+      this.setState({
+        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.COMPUTER_SYMBOL : this.state.PLAYER_ONE_SYMBOL
+      })
+    }
+  }
+
+  setTimer() {
+    if (this.state.game === "on" && this.state.currentTurn === this.state.COMPUTER_SYMBOL) {
+      setTimeout(this.updateBoard.bind(this), 1000)
+    }
+  }
+
 
   render() {
     return (
